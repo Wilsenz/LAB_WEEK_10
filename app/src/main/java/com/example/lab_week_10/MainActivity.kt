18e70e40df77d36eaa230b2eec5b4ea6cbf38ca7
wilsen <wilsensusanto54@gmail.com>
2025-11-11 21:08:20 +0700

    Commit No. 3: Integrate Room Database for Data Persistence

--- app/src/main/java/com/example/lab_week_10/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_10/MainActivity.kt
@@ -7,7 +7,13 @@ import android.widget.TextView
 import kotlin.getValue
 import com.example.lab_week_10.viewmodels.TotalViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.lab_week_10.database.TotalDatabase
+import androidx.room.Room
+import com.example.lab_week_10.database.Total
+
 class MainActivity : AppCompatActivity() {
+
+    private val db by lazy { prepareDatabase() }
     private val viewModel by lazy {
         ViewModelProvider(this)[TotalViewModel::class.java]
     }
@@ -16,9 +22,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initializeValueFromDatabase()
         prepareViewModel()
     }
 
+    override fun onPause() {
+        super.onPause()
+        db.totalDao().update(Total(ID, viewModel.total.value!!))
+    }
     private fun updateText(total: Int) {
         findViewById<TextView>(R.id.text_total).text =
             getString(R.string.text_total, total)
@@ -32,5 +43,25 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.button_increment).setOnClickListener {
             viewModel.incrementTotal()
         }
+    }
+
+    private fun prepareDatabase(): TotalDatabase {
+        return Room.databaseBuilder(
+            applicationContext,
+            TotalDatabase::class.java, "total-database"
+        ).allowMainThreadQueries().build()
+    }
+
+    private fun initializeValueFromDatabase() {
+        val total = db.totalDao().getTotal(ID)
+        if (total.isEmpty()) {
+            db.totalDao().insert(Total(id = 1, total = 0))
+        } else {
+            viewModel.setTotal(total.first().total)
+        }
+    }
+
+    companion object {
+        const val ID: Long = 1
     }
 }
